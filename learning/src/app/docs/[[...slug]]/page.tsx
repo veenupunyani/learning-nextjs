a18bf445005d5291c208315f14a9f320dcf88c13
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2025-08-15 11:49:15 -0700

    catch all segments

---
+++ learning/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,14 @@
+export default async function DocsPage({ 
+  params 
+}: { 
+  params: Promise<{ slug: string[] }> 
+}) {
+  const { slug } = await params;
+
+  return (
+    <div>
+      <h1>Documentation</h1>
+      
+    </div>
+  );
+}
