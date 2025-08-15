270ec168db7516cafd9953a0f2a3d906763fb065
Veenu Punyani <Veenu.veenu@gov.bc.ca>
2025-08-15 11:16:56 -0700

    dynamic routing

---
+++ learning/src/app/products/page.tsx
@@ -0,0 +1,14 @@
+export default function ProductsList() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Welcome to the products page!</p>
+      <h2>Available Products:</h2>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  );
+}
